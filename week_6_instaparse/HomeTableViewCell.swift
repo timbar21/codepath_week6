d1e0814b769c538752462f1860a5e04deb1cbf68
Timbar21 <timbar21pb@gmail.com>
2016-03-19 14:55:16 -0400

    fixed major bug with camera page

---
+++ week_6_instaparse/HomeTableViewCell.swift
@@ -0,0 +1,59 @@
+//
+//  TableViewCell.swift
+//  week_6_instaparse
+//
+//  Created by Tim Barnard on 3/16/16.
+//  Copyright © 2016 Tim Barnard. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var userImageView: UIImageView!
+    @IBOutlet weak var captionLabel: UILabel!
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+    let captionCheck  = "Enter caption..."
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    var postsObject: PFObject? {
+        didSet {
+            post = Post(postObject: postsObject!)
+            post.cell = self;
+        }
+    }
+    
+    var post: Post! {
+        didSet {
+            userImageView.image = post.photo
+            print("This is the image that is to be set: \(post.photo)")
+            // this causes the nil crash!! so userPost.photo must be nil...
+            //            print("This is the image thatx is to be set: \((post.photo)!)")
+            
+            
+            if post.caption == captionCheck {
+                captionLabel.text = ""
+            } else {
+                captionLabel.text = post.caption
+            }
+            print("the caption is: \((captionLabel.text)!)")
+            
+            usernameLabel.text = post.author?.username
+            print("the user is: \((usernameLabel.text)!)")
+            print("")
+        }
+    }
+    
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ week_6_instaparse/Post.swift
@@ -0,0 +1,13 @@
+//
+//  Post.swift
+//  week_6_instaparse
+//
+//  Created by Tim Barnard on 3/16/16.
+//  Copyright © 2016 Tim Barnard. All rights reserved.
+//
+
+import UIKit
+
+class Post: NSObject {
+
+}
